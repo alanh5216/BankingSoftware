4f0de73c13b8d446e1cde2a1383d4f3b13599cf7
alanh5216 <111913523+alanh5216@users.noreply.github.com>
2025-06-04 23:31:02 -0700

    added prev.txt to save an older version of code, also mainframe.cpp now correctly can resize and keep everything including the image proportional to one another

--- Mainframe.cpp
+++ Mainframe.cpp
@@ -1,34 +1,68 @@
 #include "Mainframe.h"
 #include <wx/wx.h>
+#include <wx/statbmp.h>
+#include <wx/sizer.h>
+#include <wx/mstream.h>
 
-MainFrame::MainFrame(const wxString& Title) : wxFrame(nullptr, wxID_ANY, Title) {
+MainFrame::MainFrame(const wxString& Title) : wxFrame(nullptr, wxID_ANY, Title, wxDefaultPosition, wxSize(800, 600)) {
+    wxImage::AddHandler(new wxPNGHandler);
 
+    // Panels
     wxPanel* panel_top = new wxPanel(this);
     wxPanel* panel_middle_left = new wxPanel(this);
     wxPanel* panel_middle_right = new wxPanel(this);
     wxPanel* panel_bottom = new wxPanel(this);
 
-    wxBoxSizer* sizer_vertical = new wxBoxSizer(wxVERTICAL);
-    wxBoxSizer* sizer_horizontal = new wxBoxSizer(wxHORIZONTAL);
-    wxBoxSizer* sizer_middle_left = new wxBoxSizer(wxVERTICAL);
-    wxBoxSizer* sizer_middle_right = new wxBoxSizer(wxVERTICAL);
+    // Sizers
+    wxBoxSizer* mainSizer = new wxBoxSizer(wxVERTICAL);
+    wxBoxSizer* middleSizer = new wxBoxSizer(wxHORIZONTAL);
+    wxBoxSizer* leftSizer = new wxBoxSizer(wxVERTICAL);
+    wxBoxSizer* rightSizer = new wxBoxSizer(wxVERTICAL);
 
+    // Set background colors
+    panel_top->SetBackgroundColour(wxColor(50, 50, 50));
+    panel_middle_left->SetBackgroundColour(wxColor(100, 100, 100));
+    panel_middle_right->SetBackgroundColour(wxColor(150, 150, 150));
+    panel_bottom->SetBackgroundColour(wxColor(50, 50, 50));
+
+    // Top text
+    wxStaticText* topText = new wxStaticText(panel_top, wxID_ANY, "Top Panel Text");
+    topText->SetForegroundColour(*wxWHITE);
+    wxBoxSizer* topSizer = new wxBoxSizer(wxVERTICAL);
+    topSizer->Add(topText, 1, wxALIGN_CENTER | wxALL, 10);
+    panel_top->SetSizer(topSizer);
+
+    // Bottom text
+    wxStaticText* bottomText = new wxStaticText(panel_bottom, wxID_ANY, "Bottom Panel Text");
+    bottomText->SetForegroundColour(*wxWHITE);
+    wxBoxSizer* bottomSizer = new wxBoxSizer(wxVERTICAL);
+    bottomSizer->Add(bottomText, 1, wxALIGN_CENTER | wxALL, 10);
+    panel_bottom->SetSizer(bottomSizer);
+
+    // Load and add image
+    wxBitmap logoBitmap("Documents/BankingSoftware/logo.png", wxBITMAP_TYPE_PNG);
+    wxBitmapBundle logoBundle(logoBitmap);
+    wxStaticBitmap* logoWidget = new wxStaticBitmap(panel_middle_left, wxID_ANY, logoBundle);
+    logoWidget->SetScaleMode(wxStaticBitmap::Scale_AspectFit);
+    logoWidget->SetMinSize(wxSize(0, 0)); // Let the image scale down
+
+    leftSizer->Add(logoWidget, 1, wxEXPAND | wxALL, 10);
+    panel_middle_left->SetSizer(leftSizer);
+
+    // Right panel content
     wxTextCtrl* textCtrl = new wxTextCtrl(panel_middle_right, wxID_ANY, "TextTextTextText", wxDefaultPosition, wxDefaultSize, wxTE_PASSWORD);
+    rightSizer->Add(textCtrl, 0, wxALL | wxALIGN_CENTER, 10);
+    panel_middle_right->SetSizer(rightSizer);
 
-    panel_top           ->SetBackgroundColour(wxColor(50,50,50));
-    panel_middle_left   ->SetBackgroundColour(wxColor(100,100,100));
-    panel_middle_right  ->SetBackgroundColour(wxColor(100,100,100));
-    panel_bottom        ->SetBackgroundColour(wxColor(50,50,50));
+    // Assemble middle
+    middleSizer->Add(panel_middle_left, 1, wxEXPAND);
+    middleSizer->Add(panel_middle_right, 1, wxEXPAND);
 
-    sizer_vertical      ->Add(panel_top,1, wxEXPAND);
-    sizer_horizontal    ->Add(panel_middle_left,1, wxEXPAND);
-    sizer_horizontal    ->Add(panel_middle_right,1, wxEXPAND);
-    sizer_vertical      ->Add(sizer_horizontal,3, wxEXPAND);
-    sizer_vertical      ->Add(panel_bottom,1, wxEXPAND);
-    sizer_middle_right  ->Add(textCtrl, 0, wxALL | wxALIGN_CENTER, 10);
+    // Assemble all
+    mainSizer->Add(panel_top, 20, wxEXPAND);         // 15% of total height
+    mainSizer->Add(middleSizer, 60, wxEXPAND);       // 70% of total height
+    mainSizer->Add(panel_bottom, 20, wxEXPAND);      // 15% of total height
 
-    panel_middle_right->SetSizer(sizer_middle_right);
-
-    this->SetSizer(sizer_vertical);
+    this->SetSizer(mainSizer); //if use SetSizerAndFit then it will make it so that it can only go as small as the text control can be as small
     this->Layout();
 }
